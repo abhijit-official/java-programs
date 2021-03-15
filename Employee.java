3d9ba03ef4b159b7917a044a928c1c7106fcab64
abhijit-official <abhijitchavan1717@gmail.com>
2021-03-16 01:26:52 +0530

    added getter and setters

--- Employee.java
+++ Employee.java
@@ -7,7 +7,6 @@ public class Employee{
         this.number=number;
         this.name=name;
         this.salary=salary;
-
     }
     public void display (){
         System.out.println("roll number is "+ number);
@@ -17,6 +16,13 @@ public class Employee{
     public static void main(String[] args) {
         Employee e1 = new Employee(123, "abhijit", 223.8);
         e1.display();
+
+        // Student Object created from Student class in Employee with getter and setters.
+        // accessing private vars
+        Student s2 = new Student();
+        s2.setName("asdasd");
+        System.out.println(s2.getName());
+
     }
         
 }
